a4a346d7d185607271b2f18adcf0b61dcb0e78ae
Ivan583 <ivanych2293@gmail.com>
2024-02-02 13:03:57 +0300

    add new flight

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import FlightList from "./components/FlightList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
@@ -10,12 +10,35 @@ const [flights, setFlights] = useState([
   {id: 1, driver: 'Bolo Yeung', weight: 5900},
 ])
 
+const [driver, setDriver] = useState('')
+const [weight, setWeight] = useState(0)
+
+const addNewFlight = e => {
+  e.preventDefault();
+  const newFlight = {
+    id: Date.now(),
+    driver,
+    weight
+  };
+  setFlights([...flights, newFlight]);
+}
+
   return (
     <div className="App">
       <form>
-        <MyInput type="text" placeholder="driver" />
-        <MyInput type="number" placeholder="0" />
-        <MyButton>Add flight</MyButton>
+        <MyInput 
+        value={driver}
+        onChange={e => setDriver(e.target.value)}
+        type="text" 
+        placeholder="driver" />
+
+        <MyInput 
+        value={weight} 
+        onChange={e => setWeight(parseInt(e.target.value))} 
+        type="number" 
+        placeholder="weight" />
+
+        <MyButton onClick={addNewFlight}>Add flight</MyButton>
       </form>
 
       <FlightList flights={flights} />

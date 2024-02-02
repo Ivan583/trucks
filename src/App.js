2a227ee7176062bf3b634bcb04e634f08221388e
Ivan583 <ivanych2293@gmail.com>
2024-02-02 15:50:48 +0300

    подключение модального окна

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import FlightList from "./components/FlightList";
 import FlightForm from "./components/FlightForm";
+import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/modal/MyModal";
 
 function App() {
 const [flights, setFlights] = useState([
@@ -9,12 +11,24 @@ const [flights, setFlights] = useState([
   {id: 1, driver: 'Bolo Yeung', weight: 5900},
 ])
 
-const createFlight = newFlight =>
-setFlights([...flights, newFlight]);
+const [modal, setModal] = useState(false);
+
+const createFlight = newFlight => {
+  setFlights([...flights, newFlight]);
+  setModal(false);
+}
 
   return (
     <div className="App">
-      <FlightForm create={createFlight} />
+      <div className={'btns'}>
+        <MyButton onClick={() => setModal(true)} >
+          Add new flight
+        </MyButton>
+      </div>
+
+      <MyModal visible={modal} setVisible={setModal} >
+        <FlightForm create={createFlight} />
+      </MyModal>      
 
       <FlightList flights={flights} />
     </div>
